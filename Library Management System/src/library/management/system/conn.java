2a8af1bedcc8a032c2d44ac26049033a1940e08c
Deepak Kumar Mahato <deepakmahatoj.n.v@gmail.com>
2021-08-12 11:03:39 +0530

    Update conn.java

--- Library Management System/src/library/management/system/conn.java
+++ Library Management System/src/library/management/system/conn.java
@@ -1,15 +1,5 @@
 
-/*
-            TCR  Innovation
-   CORE JAVA+SQL INTERNSHIP PROGRAM
-        INTERNSHIP (( FINAL PROJECT ON LIBRARY MANAGEMENT SYSTEM )) .
 
-Name -- VICKY KUMAR JHA
-EMAIL -- vk2535vk@gmail.com
-
-INTERNSHIP (( FINAL PROJECT ON LIBRARY MANAGEMENT SYSTEM ))
-
- */
 
 package library.management.system;
 
